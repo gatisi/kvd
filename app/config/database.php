14d83ec2faed5154dd2151a033fe166d6dc978a3
Gatis Innus <gatis.innus@gmail.com>
2014-05-30 17:25:11 +0300

    cartalyst/sentry user manamegment installed

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'grafix',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'asdzxc',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
